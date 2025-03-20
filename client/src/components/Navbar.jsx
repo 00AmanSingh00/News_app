6abe329530d42cfd95fa350735fba331af61c4ee
Aman Singh <00amansinghh00@gmail.com>
2025-03-20 11:57:08 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import '../App.css';
 import SearchBar from './SearchBar';
 import { Link } from 'react-router-dom';
+import Logout from './Logout';
 
-const Navbar = ({ onNavItemClick, curSelectedNav, onSearch }) => {
+const Navbar = ({ onNavItemClick, curSelectedNav, onSearch, setIsAuthenticated }) => {
   const [isPopupMenuOpen, setIsPopupMenuOpen] = useState(false);
 
   const togglePopupMenu = () => {
@@ -25,12 +26,14 @@ const Navbar = ({ onNavItemClick, curSelectedNav, onSearch }) => {
             <li className={`hover-link nav-item ${curSelectedNav === 'ipl' ? 'active' : ''}`} id="ipl" onClick={() => onNavItemClick('ipl')}>ENTERTAINMENT</li>
             <li className={`hover-link nav-item ${curSelectedNav === 'finance' ? 'active' : ''}`} id="finance" onClick={() => onNavItemClick('finance')}>FINANCE</li>
             <li className={`hover-link nav-item ${curSelectedNav === 'politics' ? 'active' : ''}`} id="politics" onClick={() => onNavItemClick('politics')}>POLITICS</li>
-
             <li className={`hover-link nav-item ${curSelectedNav === 'account' ? 'active' : ''}`} id="account">
-  <Link  to="/my-account" className="my-account-link" onClick={() => onNavItemClick('account')}>
-    ACCOUNT
-  </Link>
-</li>
+              <Link to="/my-account" className="my-account-link" onClick={() => onNavItemClick('account')}>
+                ACCOUNT
+              </Link>
+            </li>
+            <li>
+              <Logout setIsAuthenticated={setIsAuthenticated} />
+            </li>
           </ul>
         </div>
         <SearchBar onSearch={onSearch} />
@@ -45,22 +48,16 @@ const Navbar = ({ onNavItemClick, curSelectedNav, onSearch }) => {
           &times;
         </div>
         <ul>
-
-
-
-
-          
           <li className={`hover-link nav-item ${curSelectedNav === 'ipl' ? 'active' : ''}`} id="ipl" onClick={() => { onNavItemClick('ipl'); togglePopupMenu(); }}>ENTERTAINMENT</li>
           <li className={`hover-link nav-item ${curSelectedNav === 'finance' ? 'active' : ''}`} id="finance" onClick={() => { onNavItemClick('finance'); togglePopupMenu(); }}>FINANCE</li>
           <li className={`hover-link nav-item ${curSelectedNav === 'politics' ? 'active' : ''}`} id="politics" onClick={() => { onNavItemClick('politics'); togglePopupMenu(); }}>POLITICS</li>
-
-
-
-          
           <li onClick={togglePopupMenu}>
             <Link to="/my-account" className="my-account-link">
               ACCOUNT
             </Link>
+          </li>
+          <li onClick={togglePopupMenu}>
+            <Logout setIsAuthenticated={setIsAuthenticated} />
           </li>
         </ul>
       </div>
